bcd7385a7de365a8db8a90be160c677582fe7af5
Blake Fletcher <blkfltchr@gmail.com>
2018-12-30 12:21:07 -0500

    Separated widgets into different files

--- my_flutter_app/lib/main.dart
+++ my_flutter_app/lib/main.dart
@@ -1,49 +1,18 @@
 import 'package:flutter/material.dart';
 
+import './product_manager.dart';
+
 void main() => runApp(MyApp());
 
-class MyApp extends StatefulWidget {
+class MyApp extends StatelessWidget {
   @override
-  State<StatefulWidget> createState() {
-    return _MyAppState();
-  }
-}
-
-class _MyAppState extends State<MyApp> {
-  List<String> _products = ['Food tester'];
-
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(
           title: Text('EasyList'),
         ),
-        body: Column(
-          children: [
-            Container(
-              margin: EdgeInsets.all(10.0),
-              child: RaisedButton(
-                  onPressed: () {
-                    setState(() {
-                      _products.add('Advanced food tester');
-                    });
-                  },
-                  child: Text('Add product')),
-            ),
-            Column(
-              children: _products
-                  .map((element) => Card(
-                        child: Column(
-                          children: <Widget>[
-                            Image.asset('assets/food.jpg'),
-                            Text(element),
-                          ],
-                        ),
-                      ))
-                  .toList(),
-            )
-          ],
-        ),
+        body: ProductManager(),
       ),
     );
   }
