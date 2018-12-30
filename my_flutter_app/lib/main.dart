5d217fc5d69356652ebef5ac373cc370e2e13d00
Blake Fletcher <blkfltchr@gmail.com>
2018-12-30 11:19:38 -0500

    Mapped over list and add product functioning

--- my_flutter_app/lib/main.dart
+++ my_flutter_app/lib/main.dart
@@ -2,8 +2,16 @@ import 'package:flutter/material.dart';
 
 void main() => runApp(MyApp());
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
   @override
+  State<StatefulWidget> createState() {
+    return _MyAppState();
+  }
+}
+
+class _MyAppState extends State<MyApp> {
+  List<String> _products = ['Food tester'];
+
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
@@ -15,25 +23,25 @@ class MyApp extends StatelessWidget {
             Container(
               margin: EdgeInsets.all(10.0),
               child: RaisedButton(
-                onPressed: () {}, 
-                child: Text('Add product')),
+                  onPressed: () {
+                    setState(() {
+                      _products.add('Advanced food tester');
+                    });
+                  },
+                  child: Text('Add product')),
             ),
-            Card(
-              child: Column(
-                children: <Widget>[
-                  Image.asset('assets/food.jpg'),
-                  Text('Food paradise'),
-                ],
-              ),
-            ),
-            Card(
-              child: Column(
-                children: <Widget>[
-                  Image.asset('assets/food.jpg'),
-                  Text('Food paradise'),
-                ],
-              ),
-            ),
+            Column(
+              children: _products
+                  .map((element) => Card(
+                        child: Column(
+                          children: <Widget>[
+                            Image.asset('assets/food.jpg'),
+                            Text(element),
+                          ],
+                        ),
+                      ))
+                  .toList(),
+            )
           ],
         ),
       ),
