a091b2c2c771cb1d4d37881c76037aa389bbddf5
Blake Fletcher <blkfltchr@gmail.com>
2019-01-02 10:00:06 -0500

    Added dummy login page

--- my_flutter_app/lib/main.dart
+++ my_flutter_app/lib/main.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 
-import './pages/home.dart';
+import './pages/auth.dart';
 
 void main() {
   runApp(MyApp());
@@ -14,7 +14,7 @@ class MyApp extends StatelessWidget {
           brightness: Brightness.light,
           primarySwatch: Colors.deepOrange,
           accentColor: Colors.blueAccent),
-      home: HomePage(),
+      home: AuthPage(),
     );
   }
 }
